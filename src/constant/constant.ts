913b4c444661ed89fbd5a40ba303ee6bb93dacf2
vivekkumar5555 <143058399+vivekkumar5555@users.noreply.github.com>
2025-07-29 00:19:38 +0530

    Update constant.ts

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -1,2 +1,4 @@
-export const base_auth_url ="http://localhost:5000/api/auth";
-export const base_url="http://localhost:5000/api"
+// export const base_auth_url ="http://localhost:5000/api/auth";
+// export const base_url="http://localhost:5000/api"
+
+export const base_auth_url ="https://note-app-k4fn.onrender.com/api/auth"
